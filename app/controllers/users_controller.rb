a7442c2b27f31e43dd30881a1dee0243f1832ed7
Hugo Suastegui <hugosuaspi_@hotmail.com>
2016-08-11 17:57:44 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  def show
+  	@user = User.find(params[:id])
+  	# The next ruby gem is used to find and correct bugs as the program is developing in te console. To release the debugger Ctrl + D. See Hartl Tutorial section 7.1.2 for more info :).
+  	# debugger
   end
+
+  def new
+  	@user = User.new
+  end
+
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+      flash[:success] = "Welcome to the Sample App"
+      redirect_to @user
+      # Why does @user redirects to users/show/id?
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+	  def user_params
+	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	  end
+
 end
--- db/migrate/20160807224906_add_index_to_users_email.rb
+++ db/migrate/20160807224906_add_index_to_users_email.rb
@@ -1,6 +1,6 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[5.0]
   def change
-  	# The following method prevents duplicated elements be saved into the database. it works like a book index that prevents doble indexed elements under the same index number.
+  	# The following method prevents duplicated elements to be saved into the database. it works like a book index that prevents doble indexed elements under the same index number.
   	add_index :users, :email, unique: true
   end
   # The fixtures/users.yml contain sample data for the test database, so in order for the test to pass, we must delete the whole file.
